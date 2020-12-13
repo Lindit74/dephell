45f2a4ef0cbab4b5e38512be7d5f8bd8fac2f2ca
LINDITA LOHJA <75851253+Lindit74@users.noreply.github.com>
2020-12-13 15:15:09 +0100

    Add files via upload

---
+++ Presenting myself.py
@@ -0,0 +1,9 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("LINDITA LOHJA")
+LINDITA LOHJA
+>>> print("Teacher of Physics and informatics ")
+Teacher of Physics and informatics 
+>>> print("Using different tools in education for trainning teachers to implemetn technology in education exspecially physics")
+Using different tools in education for trainning teachers to implemetn technology in education exspecially physics
+>>> 
